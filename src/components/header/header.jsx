9c2b10c9ce5187a09ce3f2c95749e14b6d048d6b
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-15 11:07:14 +0530

    update the header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,25 @@
+import logo from "../../assets/snap.svg";
 const Header = () => {
   return (
     <div>
-      <div></div>
-      <div></div>
       <div>
-        <div></div>
-        <div></div>
+        <img src={logo} alt="" />
+      </div>
+      <div>
+        <div>
+          <div>Features</div>
+          <div></div>
+        </div>
+        <div>
+          <div>Company</div>
+          <div></div>
+        </div>
+        <div>Careers</div>
+        <div>About</div>
+      </div>
+      <div>
+        <div>Login</div>
+        <div className="">Register</div>
       </div>
     </div>
   );
