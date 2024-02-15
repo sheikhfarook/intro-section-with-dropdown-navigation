f4d66a702474e2e3d7ce225b64be4285243e36b2
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-15 12:32:32 +0530

    complete the header part

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,27 +2,33 @@ import logo from "../../assets/snap.svg";
 
 const Header = () => {
   return (
-    <div>
-      <div>
-        <img src={logo} alt="" />
-      </div>
-      <div>
+    <div
+      className="flex justify-between gap-10 m-[3%] font-epilogue
+    text-[14px] font-[500] text-[#686868]">
+      <div className="flex gap-20">
         <div>
-          <div>Features</div>
-          <div></div>
+          <img src={logo} alt="" />
         </div>
-        <div>
-          <div>Company</div>
+        <div className="flex gap-10">
           <div>
-            <img src="" alt="" />
+            <div className="cursor-pointer hover:text-[#151515]">Features</div>
           </div>
+          <div>
+            <div className="cursor-pointer hover:text-[#151515]">Company</div>
+            <div></div>
+          </div>
+          <div className="cursor-pointer hover:text-[#151515]">Careers</div>
+          <div className="cursor-pointer hover:text-[#151515]">About</div>
         </div>
-        <div>Careers</div>
-        <div>About</div>
       </div>
-      <div>
-        <div>Login</div>
-        <div className="">Register</div>
+      <div className="flex gap-10">
+        <div className="ml-auto cursor-pointer hover:text-[#151515]">Login</div>
+        <div
+          className="cursor-pointer w-[104px] h-[42px] py-[8.9px] mt-[-10px] text-center 
+          rounded-[14px] border-[#686868] border-[1.5px] hover:text-[#151515] 
+          hover:border-[#151515] ">
+          Register
+        </div>
       </div>
     </div>
   );
