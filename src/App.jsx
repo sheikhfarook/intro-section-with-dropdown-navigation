4bb7ac34500138a32adc7c85dfe2045f9ab180e5
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-15 12:33:10 +0530

    import the new components file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/header/header";
+import RemoteWork from "./components/pages/remote-work";
 
 function App() {
   return (
     <div>
       <Header />
+      <RemoteWork />
     </div>
   );
 }
