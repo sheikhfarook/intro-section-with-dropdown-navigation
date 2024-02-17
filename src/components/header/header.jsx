c3e1cff8d652ba15624deef09accd3929bfb9512
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-17 09:58:27 +0530

    complete the task

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,33 +1,238 @@
 import logo from "../../assets/snap.svg";
+import menu from "../../assets/menu.svg";
+import arrowdown from "../../assets/arrow-down.svg";
+// import arrowup from "../../assets/arrow-up.svg";
+import todo from "../../assets/todo.svg";
+import calendar from "../../assets/calendar.svg";
+import plan from "../../assets/planning.svg";
+import reminder from "../../assets/reminders.svg";
+
+import {
+  Drawer,
+  DrawerBody,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Button,
+} from "@chakra-ui/react";
+import { Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+} from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/react";
+import React from "react";
 
 const Header = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
+
   return (
-    <div
-      className="flex justify-between gap-10 m-[3%] font-epilogue
-    text-[14px] font-[500] text-[#686868]">
-      <div className="flex gap-20">
+    <div className="flex justify-between m-[3%] font-epilogue text-[14px] font-[00] text-[#686868]">
+      <div className="flex gap-[5rem]">
         <div>
           <img src={logo} alt="" />
         </div>
-        <div className="flex gap-10">
-          <div>
-            <div className="cursor-pointer hover:text-[#151515]">Features</div>
-          </div>
-          <div>
-            <div className="cursor-pointer hover:text-[#151515]">Company</div>
-            <div></div>
-          </div>
-          <div className="cursor-pointer hover:text-[#151515]">Careers</div>
-          <div className="cursor-pointer hover:text-[#151515]">About</div>
+        {/* charka */}
+        <div className="ml-[8rem] mt-[-0.5rem] mb-10 md:hidden">
+          <Button ref={btnRef} colorScheme="white" onClick={onOpen}>
+            <img src={menu} alt="" />
+          </Button>
+          <Drawer
+            isOpen={isOpen}
+            placement="right"
+            onClose={onClose}
+            finalFocusRef={btnRef}>
+            <DrawerOverlay />
+            <DrawerContent>
+              <DrawerCloseButton />
+              <DrawerBody>
+                <div className="text-[16px] font-[500] mt-20 space-y-5">
+                  <div className="space-y-5">
+                    <div>
+                      <Accordion allowMultiple>
+                        <AccordionItem>
+                          <h2>
+                            <AccordionButton>
+                              <Box>Features</Box>
+                              <AccordionIcon />
+                            </AccordionButton>
+                          </h2>
+                          <AccordionPanel lineHeight={10} marginLeft={4}>
+                            <div className="font-epilogue">
+                              <div className="flex gap-4 items-center">
+                                <div>
+                                  <img src={todo} alt="" />
+                                </div>
+                                <div>Todo List</div>
+                              </div>
+                              <div className="flex gap-4 items-center">
+                                <div>
+                                  <img src={calendar} alt="" />
+                                </div>
+                                <div>Calendar</div>
+                              </div>
+                              <div className="flex gap-4 items-center">
+                                <div>
+                                  <img src={plan} alt="" />
+                                </div>
+                                <div>Reminders</div>
+                              </div>
+                              <div className="flex gap-4 items-center">
+                                <div>
+                                  <img src={reminder} alt="" />
+                                </div>
+                                <div>Planning</div>
+                              </div>
+                            </div>
+                          </AccordionPanel>
+                        </AccordionItem>
+
+                        <AccordionItem>
+                          <h2>
+                            <AccordionButton>
+                              <Box>Company</Box>
+                              <AccordionIcon />
+                            </AccordionButton>
+                          </h2>
+                          <AccordionPanel lineHeight={10} marginLeft={4}>
+                            <div className="font-epilogue">
+                              <div>History</div>
+                              <div>Our Team</div>
+                              <div>Blog</div>
+                            </div>
+                          </AccordionPanel>
+                        </AccordionItem>
+                      </Accordion>
+                    </div>
+                    <div className="">Careers</div>
+                    <div className="">About</div>
+                  </div>
+                  <div className="space-y-5 text-center">
+                    <div className="ml-auto ">Login</div>
+                    <div
+                      className=" w-[196px] m-auto h-[42px] py-[6px] mt-[-10px] text-center rounded-[14px] border-[#686868] border-[1.5px]
+                ">
+                      Register
+                    </div>
+                  </div>
+                </div>
+              </DrawerBody>
+            </DrawerContent>
+          </Drawer>
         </div>
-      </div>
-      <div className="flex gap-10">
-        <div className="ml-auto cursor-pointer hover:text-[#151515]">Login</div>
-        <div
-          className="cursor-pointer w-[104px] h-[42px] py-[8.9px] mt-[-10px] text-center 
-          rounded-[14px] border-[#686868] border-[1.5px] hover:text-[#151515] 
-          hover:border-[#151515] ">
-          Register
+        <div className="flex max-sm:hidden">
+          <div className="flex gap-10">
+            <div className="flex">
+              <div className="">
+                <Menu>
+                  <MenuButton>
+                    <div className="flex gap-2 cursor-pointer hover:text-[#151515]">
+                      <p>Features</p>
+                      <div className="m-auto">
+                        <img
+                          className="w-3 h-2 items-center"
+                          src={arrowdown}
+                          alt=""
+                        />
+                      </div>
+                      {/* <div className="m-auto">
+                        <img
+                          className="w-3 h-2 items-center"
+                          src={arrowup}
+                          alt=""
+                        />
+                      </div> */}
+                    </div>
+                  </MenuButton>
+                  <MenuList
+                    px={4}
+                    borderRadius={10}
+                    minWidth="10rem"
+                    marginLeft={-20}>
+                    <div className="font-[600]">
+                      <MenuItem>
+                        <div className="flex gap-4 items-center">
+                          <div>
+                            <img src={todo} alt="" />
+                          </div>
+                          <div>Todo List</div>
+                        </div>
+                      </MenuItem>
+                      <MenuItem>
+                        {" "}
+                        <div className="flex gap-4 items-center">
+                          <div>
+                            <img src={calendar} alt="" />
+                          </div>
+                          <div>Calendar</div>
+                        </div>
+                      </MenuItem>
+                      <MenuItem>
+                        <div className="flex gap-4 items-center">
+                          <div>
+                            <img src={reminder} alt="" />
+                          </div>
+                          <div>Reminders</div>
+                        </div>
+                      </MenuItem>
+                      <MenuItem>
+                        <div className="flex gap-4 items-center">
+                          <div>
+                            <img src={plan} alt="" />
+                          </div>
+                          <div>Planning</div>
+                        </div>
+                      </MenuItem>
+                    </div>
+                  </MenuList>
+                </Menu>
+              </div>
+            </div>
+            <div>
+              <Menu>
+                <MenuButton>
+                  <div className="flex gap-2 cursor-pointer hover:text-[#151515]">
+                    <p>Company</p>
+                    <div className="m-auto">
+                      <img
+                        className="w-3 h-2 items-center"
+                        src={arrowdown}
+                        alt=""
+                      />
+                    </div>
+                  </div>
+                </MenuButton>
+                <MenuList
+                  px={2}
+                  borderRadius={10}
+                  minWidth="7rem"
+                  marginLeft={-8}>
+                  <div className="font-[600]">
+                    <MenuItem>History</MenuItem>
+                    <MenuItem>Our Team</MenuItem>
+                    <MenuItem>Blog</MenuItem>
+                  </div>
+                </MenuList>
+              </Menu>
+            </div>
+            <div className="cursor-pointer hover:text-[#151515]">Careers</div>
+            <div className="cursor-pointer hover:text-[#151515]">About</div>
+          </div>
+          <div className="flex gap-10 absolute right-[3%]">
+            <div className="ml-auto cursor-pointer hover:text-[#151515]">
+              Login
+            </div>
+            <div
+              className="cursor-pointer w-[104px] h-[42px] py-[8.9px] mt-[-10px] text-center rounded-[14px] border-[#686868] border-[1.5px]
+               hover:text-[#151515] hover:border-[#151515] ">
+              Register
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/pages/remote-work.jsx
+++ src/components/pages/remote-work.jsx
@@ -7,32 +7,31 @@ import hero2 from "../../assets/image-hero-mobile.png";
 
 const RemoteWork = () => {
   return (
-    <div className="flex justify-between max-sm:flex-col-reverse md:mx-[10%]">
+    <div className="flex justify-between mb-10 max-sm:flex-col-reverse md:flex-col-reverse md:mx-[10%] lg:flex-row">
       <div
-        className="mt-[6rem] font-epilogue space-y-[6.5rem] 
-        max-sm:mx-[5%] max-sm:text-center max-sm:space-y-[3rem] max-sm:mt-12  ">
+        className="mt-[6rem] font-epilogue space-y-[6.5rem] max-sm:mx-[5%] max-sm:text-center max-sm:space-y-[3rem] max-sm:mt-12 md:text-center 
+        lg:text-left ">
         <div className="md:space-y-14   ">
           <div
-            className="md:w-[530px] md:h-[161px] text-[80px] font-[700] tracking-[-1.1111112px] 
-          max-sm:text-[36px] max-sm:tracking[-10.5px] md:leading-[90px]">
+            className="font-[700] tracking-[-1.1111112px] max-sm:text-[36px] max-sm:tracking[-10.5px] md:text-[40px] md:w-[400px] md:m-auto 
+            md:leading-[40px] lg:w-[270px] lg:m-0 xl:w-[530px] xl:h-[161px] xl:leading-[90px] xl:text-[80px]">
             Make remote work
           </div>
           <div
-            className="text-[#686868] text-[18px] font-[500] leading-[28px] 
-          max-sm:text-[16px] max-sm:mt-4 md:w-[445px] md:h-[84px] ">
+            className="text-[#686868] font-[500] leading-[28px] max-sm:text-[16px] max-sm:mt-4 md:w-[600px] md:m-auto lg:m-0 lg:w-[330px] 
+          lg:h-[84px] lg:text-[15px] xl:w-[445px] xl:text-[18px]">
             Get your team in sync, no matter your location. Streamline
             processes, create team rituals, and watch productivity soar.
           </div>
           <div
-            className="w-[163px] h-[56px] rounded-[15px] bg-[#151515] text-[#FaFaFa]
-          text-center tracking-[-0.25px] py-[16px] text-[18px] font-[700] cursor-pointer 
-          hover:bg-[#FFF] hover:text-[#000] hover:border-[1px] border-[#000]
-          max-sm:m-auto max-sm:w-[163px] max-sm:h-[48px] max-sm:mt-6 max-sm:py-[11px]        
+            className="w-[163px] h-[56px] rounded-[15px] bg-[#151515] text-[#FaFaFa] text-center tracking-[-0.25px] py-[16px] text-[18px] 
+            font-[700] cursor-pointer hover:bg-[#FFF] hover:text-[#000] hover:border-[1px] border-[#000] max-sm:m-auto max-sm:w-[163px] 
+            max-sm:h-[48px] max-sm:mt-6 max-sm:py-[11px] md:m-auto lg:m-0      
           ">
             Learn more
           </div>
         </div>
-        <div className="flex gap-10 items-center">
+        <div className="grid gap-10 items-center max-sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4 ">
           <div>
             <img
               className="
@@ -53,7 +52,11 @@ const RemoteWork = () => {
         </div>
       </div>
       <div>
-        <img className="w-[480px] h-[640px] max-sm:hidden" src={hero} alt="" />
+        <img
+          className="w-[480px] h-[640px] max-sm:hidden md:m-auto lg:m-0"
+          src={hero}
+          alt=""
+        />
         <img className="w-[375px] h-[281.125px] md:hidden" src={hero2} alt="" />
       </div>
     </div>
